47d79d404bce5b37e1f80673031689258ee8c41e
kalam ahmed <kalamahmed714@gmail.com>
2020-11-21 18:37:38 +0600

    send notification by mail

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Notifications\sendNotification;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/send', function () {
+    /* User::find(1)->notify(new sendNotification); */
+    $users=User::all();
+    Notification::send($users, new sendNotification);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
