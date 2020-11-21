040f2344e8a145c2006f4d7bf44a86119cee9885
kalam ahmed <kalamahmed714@gmail.com>
2020-11-21 19:42:14 +0600

    Queueing Notifications

--- app/Notifications/sendNotification.php
+++ app/Notifications/sendNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class sendNotification extends Notification
+class sendNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
